05c349f388af9bb5195058e850969bb6808b1fbc
Tahir <tahir@evam.com>
2022-08-26 19:11:21 +0300

    initial condition added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,19 +9,21 @@ import { getStops } from "../../store/map";
 const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { stops, center } = useSelector((state) => state.map);
-  const { user } = useSelector((state) => state.auth);
+  const {stops, center} = useSelector( ( state ) => state.map );
+  const {user} = useSelector( ( state ) => state.auth );
 
-  useEffect(() => {
+  useEffect( () => {
     if (!user) {
-      navigate('/login')
+      navigate( '/login' )
     }
-    dispatch(getStops())
-  }, [])
+    dispatch( getStops() )
+  }, [] )
 
   return (
     <div>
-      <Map stops={stops} center={center}/>
+      {window.google ?
+        <Map stops={stops} center={center}/>
+        : null}
     </div>
   );
 };
